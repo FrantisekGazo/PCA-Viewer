b7e42cb3dddac16b151a7fd3f598ee37fe9745dd
FrantisekGazo <frantisek.gazo@inloop.eu>
2017-01-06 20:24:49 +0100

    Make text in app selectable

--- src/ui/menu/Menu.js
+++ src/ui/menu/Menu.js
@@ -99,6 +99,18 @@ function fileMenu(onSaveClick, onCloseClick) {
             {
                 label: 'Close',
                 click: onCloseClick
+            },
+            {
+                label: "Edit",
+                submenu: [
+                    {label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
+                    {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"},
+                    {type: "separator"},
+                    {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+                    {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+                    {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"},
+                    {label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:"}
+                ]
             }
         ]
     }
