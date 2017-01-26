e4fcfdd80fd52c38ea5487c6786926185330ee2e
Frantisek Gazo <frantisek.gazo@inloop.eu>
2017-01-26 10:32:27 +0100

    Support 3D pca

--- src/ui/components/ScatterPlot.jsx
+++ src/ui/components/ScatterPlot.jsx
@@ -5,6 +5,33 @@ const Plotly = require("plotly.js/dist/plotly.js");
 const { hexToRgbString } = require('../../util/ColorUtil');
 
 
+const layout = {
+    // title: 'Scatter Plot',
+    hovermode: 'closest',
+    margin: {
+        l: 30,
+        r: 30,
+        b: 30,
+        t: 30
+    },
+    xaxis: {
+        zeroline: true,
+        title: "X"
+    },
+    yaxis: {
+        zeroline: true,
+        title: "Y"
+    },
+    zaxis: {
+        zeroline: true,
+        title: "Z"
+    }
+};
+
+const opts = {
+    displayModeBar: true
+};
+
 class ScatterPlot extends React.Component {
 
     drawPlot() {
@@ -16,9 +43,11 @@ class ScatterPlot extends React.Component {
         const usedIndexY = usedValues[1];
         const usedIndexZ = usedValues[2];
 
+        let data = [];
+
         if (usedIndexX !== undefined && usedIndexY !== undefined && usedIndexZ === undefined) {
             // show 2D plot
-            const data = this.props.entries.map(entry => {
+            data = this.props.entries.map(entry => {
                 return {
                     name: entry.name,
                     x: [entry.value[usedIndexX]],
@@ -27,42 +56,38 @@ class ScatterPlot extends React.Component {
                     type: 'scatter',
                     marker: {
                         color: hexToRgbString(entry.color),
-                        size: 12
+                        size: 8
                     }
                 }
             });
-
-            const layout = {
-                title: 'Scatter Plot',
-                hovermode: 'closest',
-                margin: 30,
-                xaxis: {
-                    zeroline: true,
-                    title: "X axis"
-                },
-                yaxis: {
-                    zeroline: true,
-                    title: "Y axis"
-                }
-            };
-
-            const opts = {
-                displayModeBar: true
-            };
-
-            Plotly.newPlot(
-                elementId,
-                data,
-                layout,
-                opts
-            );
         } else if (usedIndexX !== undefined && usedIndexY !== undefined && usedIndexZ !== undefined) {
             // show 3D plot
-            console.error('TODO - implement 3D plot support');
+            data = this.props.entries.map(entry => {
+                return {
+                    name: entry.name,
+                    x: [entry.value[usedIndexX]],
+                    y: [entry.value[usedIndexY]],
+                    z: [entry.value[usedIndexZ]],
+                    mode: 'markers',
+                    type: 'scatter3d',
+                    marker: {
+                        color: hexToRgbString(entry.color),
+                        size: 6
+                    }
+                }
+            });
         } else {
             // other number is not supported
             console.error('Only 2D and 3D plot is supported', usedValues);
+            return;
         }
+
+        Plotly.newPlot(
+            elementId,
+            data,
+            layout,
+            opts
+        );
     }
 
     componentDidMount() {
