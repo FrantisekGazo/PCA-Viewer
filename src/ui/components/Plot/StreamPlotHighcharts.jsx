4f3d281113c9608a67bebc63de426ca434a0b54b
Frantisek Gazo <frantisek.gazo@inloop.eu>
2017-01-28 19:48:49 +0100

    Add new implementation for SteamPlot using Highcharts

---
+++ src/ui/components/Plot/StreamPlotHighcharts.jsx
@@ -0,0 +1,68 @@
+"use strict";
+
+const React = require('react');
+const Highcharts = require('highcharts'); // this has to be here
+const ReactHighstock = require('react-highcharts/ReactHighstock');
+
+
+class StreamPlot extends React.Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.stream !== nextProps.stream;
+    }
+
+    render() {
+        const { stream } = this.props;
+
+        if (stream.length == 0) {
+            return null;
+        }
+
+        const config = {
+            // use point number instead of timestamp
+            navigator: {
+                xAxis: {
+                    labels: {
+                        formatter: function() {
+                            return this.value;
+                        }
+                    }
+                }
+            },
+            xAxis: {
+                labels: {
+                    formatter: function() {
+                        return this.value;
+                    }
+                }
+            },
+            // change tooltip
+            tooltip: {
+                formatter: function () {
+                    return `<strong>Point:</strong> ${this.x}<br/><strong>Value:</strong> ${Math.round(this.y * 100) / 100}`;
+                }
+            },
+            // hide zoom options
+            rangeSelector: {
+                enabled: false
+            },
+            // data
+            series: [{
+                name: 'Data stream',
+                data: stream
+            }]
+        };
+
+        return (
+            <div>
+                <ReactHighstock config={config}/>
+            </div>
+        );
+    }
+}
+
+StreamPlot.propTypes = {
+    stream: React.PropTypes.array.isRequired
+};
+
+module.exports = StreamPlot;
--- src/ui/components/Plot/index.js
+++ src/ui/components/Plot/index.js
@@ -3,7 +3,7 @@
 const EigenvaluesPlot = require('./EigenvaluesPlot.jsx');
 const EntrySpectrumPlot = require('./EntrySpectrumPlot.jsx');
 const ScatterPlot = require('./ScatterPlot.jsx');
-const StreamPlot = require('./StreamPlot.jsx');
+const StreamPlot = require('./StreamPlotHighcharts.jsx');
 
 module.exports = {
     EigenvaluesPlot,
