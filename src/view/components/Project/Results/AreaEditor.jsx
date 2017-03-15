b20d7bedad13b55e617ac661ba8b383e0572a8de
Frantisek Gazo <frantisek.gazo@inloop.eu>
2017-03-15 19:40:49 +0100

    Update the AreaEditor

--- src/view/components/Project/Results/AreaEditor.jsx
+++ src/view/components/Project/Results/AreaEditor.jsx
@@ -1,7 +1,12 @@
 "use strict";
 
 const React = require('react');
-const {Card} = require('material-ui/Card');
+const { CardActions } = require('material-ui/Card');
+const IconButton = require('material-ui/IconButton').default;
+const IconVisible = require('material-ui/svg-icons/action/visibility').default;
+const IconInvisible = require('material-ui/svg-icons/action/visibility-off').default;
+const RaisedButton = require('material-ui/RaisedButton').default;
+const TextField = require('material-ui/TextField').default;
 
 
 /**
@@ -9,17 +14,49 @@ const {Card} = require('material-ui/Card');
  */
 class AreaEditor extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.k = props.coefficient;
+    }
+
+    handleCoefChange(event, newValue) {
+        const number = parseFloat(newValue);
+        if (!isNaN(number)) {
+            this.k = number;
+        }
+    }
+
+    handleUpdateClick() {
+        this.props.onCoefficientChange(this.k);
+    }
+
+    handleToggleShown() {
+        this.props.onShownChange(!this.props.shown);
+    }
+
     render() {
-        const {
-            coefficient, shown,
-            onShownChange, onCoefficientChange
-        } = this.props;
+        const { shown } = this.props;
 
         return (
-            <div>
-                k = { coefficient }
-                [{ shown ? "shown" : "hidden" }]
-            </div>
+            <CardActions>
+                <IconButton
+                    onTouchTap={this.handleToggleShown.bind(this)}>
+                    {
+                        shown ? <IconVisible/> : <IconInvisible/>
+                    }
+                </IconButton>
+
+                <TextField
+                    disabled={!shown}
+                    floatingLabelText='k'
+                    defaultValue={`${this.k}`}
+                    onChange={this.handleCoefChange.bind(this)}/>
+
+                <RaisedButton
+                    disabled={!shown}
+                    label='Update'
+                    onTouchTap={this.handleUpdateClick.bind(this)}/>
+            </CardActions>
         );
     }
 }
